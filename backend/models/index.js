34c0f13a652cf170b9cda11a66a295a3640dd0c9
CodingAniket <158985544+CodingAniket@users.noreply.github.com>
2025-06-13 13:58:46 +0530

    Delete backend directory

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-// models/index.js
-const mongoose = require('mongoose');
-const userSchema = require('./User');
-
-const User = mongoose.models.User || mongoose.model('User', userSchema);
-
-module.exports = {
-  User
-};
